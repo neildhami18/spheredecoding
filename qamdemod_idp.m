25b2877e18b23bfe9369ba747e5865e4673878ad
neildhami18 <61121403+neildhami18@users.noreply.github.com>
2020-08-04 13:26:30 +0530

    Add files via upload

---
+++ qamdemod_idp.m
@@ -0,0 +1,21 @@
+function demodData = qamdemod_idp(data,M,symMap)
+%M = 16;
+%symMap       = [11 10 14 15 9 8 12 13 1 0 4 5 3 2 6 7];
+%data            = [1.000000000000000 - 3.000000000000000i;3.000000000000000 + 3.000000000000000i;3.000000000000000 - 1.000000000000000i;1.000000000000000 + 3.000000000000000i];
+
+Re = -(sqrt(M)-1):2:(sqrt(M)-1);
+Im = 1j*Re;
+
+%signal space
+sigspace = repmat(Re,4,1);
+sigspace = sigspace(:) + repmat(Im,1,4)';
+
+symbs = zeros(size(data,1),1);
+
+for i = 1:size(data,1)
+    symbs(i) = symMap(double(find(sigspace==data(i))));
+    
+end
+
+demodData = de2bi(symbs,sqrt(M));
+demodData = flip(demodData,2);
---
+++ qammod_idp.m
@@ -0,0 +1,29 @@
+function modData = qammod_idp(data,M,symMap)
+%M = 16;
+%symMap       = [11 10 14 15 9 8 12 13 1 0 4 5 3 2 6 7];
+%data            = randi([0 1],16,1);
+
+
+data = reshape(data,[bps, size(data,1)/sqrt(M)]);
+data = data';
+symbs = bi2de(data,'left-msb');
+
+Re = -(sqrt(M)-1):2:(sqrt(M)-1);
+Im = 1j*Re;
+
+%signal space
+sigspace = repmat(Re,4,1);
+sigspace = sigspace(:) + repmat(Im,1,4)';
+
+
+modData = zeros(size(symbs,1),1);
+%symMap = symMap + 1;
+
+for i = 1:size(symbs,1)
+    modData(i) = double(symMap==symbs(i))*sigspace;
+    
+end
+
+
+
+end
